9575e8b7f75cf31da3713fd08dba5fcda399f955
adigayung <151894857+adigayung@users.noreply.github.com>
2024-07-22 14:02:31 +0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,13 +2,12 @@ from deep_translator import GoogleTranslator
 from langdetect import detect
 import traceback
 
-class CLIPTextAutoTranslate:
+class AT_CLIPTextAutoTranslate:
     @classmethod
     def INPUT_TYPES(s):
         return {"required": {"text": ("STRING", {"multiline": True, "dynamicPrompts": True}), "clip": ("CLIP", )}}
     RETURN_TYPES = ("CONDITIONING",)
     FUNCTION = "encode"
-
     CATEGORY = "conditioning"
 
     def encode(self, clip, text):
@@ -26,11 +25,34 @@ class CLIPTextAutoTranslate:
         cond, pooled = clip.encode_from_tokens(tokens, return_pooled=True)
         return ([[cond, {"pooled_output": pooled}]], )
 
+class AT_PromptText:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {"prompt": ("STRING", {"default": "prompt", "multiline": True})}}
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("prompt",)
+    FUNCTION = "get_value"
+    CATEGORY = "conditioning"
+
+    def get_value(self, prompt):
+        if prompt.strip():
+            detected_lang = detect(prompt)
+            if detected_lang != 'en':
+                try:
+                    translator = GoogleTranslator(source='auto', target='en')
+                    prompt = translator.translate(prompt)
+                except Exception as e:
+                    print(f"Translation error: {e}")
+                    traceback.print_exc()
+        return (prompt,)
+
 # Daftar node yang ingin diekspor dengan nama mereka
 NODE_CLASS_MAPPINGS = {
-    "CLIP Text Encode (Auto Translate)": CLIPTextAutoTranslate
+    "CLIP Text Encode (Auto Translate)": AT_CLIPTextAutoTranslate,
+    "Prompt Text (Auto Translate)": AT_PromptText,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "CLIPTextAutoTranslate": "CLIP Text Encode (Auto Translate)",
+    "AT_CLIPTextAutoTranslate": "CLIP Text Encode (Auto Translate)",
+    "AT_PromptText": "Prompt Text (Auto Translate)",
 }
